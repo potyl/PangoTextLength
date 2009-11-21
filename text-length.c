7fe5b011f9dbbdc23695b268651a8aa79a8f4601
Emmanuel Rodriguez <erodriguez@emo-2.local>
2009-11-22 00:04:55 +0100

    Rename font_name to font

--- text-length.c
+++ text-length.c
@@ -3,29 +3,30 @@
 #include <pango/pangocairo.h>
 #include <cairo/cairo.h>
 
-static void compute_text_length (gchar *font_name, gchar *text, int *width, int *height);
+static void compute_text_length (char *font, char *text, int *width, int *height);
 
 int main (int argc, char **argv) {
 	
 	if (argc != 3) {
 		g_print("Usage: font text\n");
+		g_print("Usage: font text [filename]\n");
 		return 1;
 	}
-	char *font_name = argv[1];
+	char *font = argv[1];
 	char *text = argv[2];
 
 	
 	g_type_init();
 
 	int width, height;
-	compute_text_length(font_name, text, &width, &height);
-	g_print("Converting %s using font %s %dx%d\n", text, font_name, width, height);
+	compute_text_length(font, text, &width, &height);
+	g_print("Converting %s using font %s %dx%d\n", text, font, width, height);
 
 	return 0;
 }
 
 
-static void compute_text_length (char *font_name, char *text, int *width, int *height) {
+static void compute_text_length (char *font, char *text, int *width, int *height) {
 
 	cairo_surface_t* surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, 0, 0);
 	cairo_t *cr = cairo_create(surface);
@@ -34,7 +35,7 @@ static void compute_text_length (char *font_name, char *text, int *width, int *h
 	PangoLayout *layout = pango_cairo_create_layout(cr);
 	pango_layout_set_text(layout, text, -1);
 
-	PangoFontDescription *desc = pango_font_description_from_string(font_name);
+	PangoFontDescription *desc = pango_font_description_from_string(font);
 	pango_layout_set_font_description(layout, desc);
 	pango_font_description_free(desc);
 	
